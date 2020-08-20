59a5f4fb6c556f3a27cabce390de40f16ad3b0b0
Katie Le <lethikatie@gmail.com>
2020-08-20 21:55:10 +0000

    add

--- lib/top_ten_anime.rb
+++ lib/top_ten_anime.rb
@@ -1,6 +1,7 @@
 require_relative "./top_ten_anime/version"
 require_relative "./top_ten_anime/cli"
 
+require 'pry'
 module TopTenAnime
   class Error < StandardError; end
   # Your code goes here...

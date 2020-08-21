f3f2a207cb74f1af535a596d6bb98f2d51da3d8f
Katie Le <lethikatie@gmail.com>
2020-08-21 08:23:55 +0000

    add

--- lib/top_ten_anime/scraper.rb
+++ lib/top_ten_anime/scraper.rb
@@ -1,7 +1,3 @@
-require ‘nokogiri’
-require ‘httparty’
-require ‘pry’
-
 class TopTenAnime::Scraper
   def get_page
     Nokogiri::HTML(open("https://myanimelist.net/topanime.php?type=bypopularity"))

df020f00900753b11adb7a6a7ce755cebcf9506f
Katie Le <lethikatie@gmail.com>
2020-08-21 09:53:21 +0000

    add

--- lib/top_ten_anime.rb
+++ lib/top_ten_anime.rb
@@ -3,7 +3,7 @@ require_relative "./top_ten_anime/cli"
 require_relative "./top_ten_anime/scraper"
 require_relative "./top_ten_anime/anime"
 
-
+require 'uri'
 require 'pry'
 require 'httparty'
 module TopTenAnime

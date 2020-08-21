84117861e8bacceec26299147b7d00a936c14ed4
Katie Le <lethikatie@gmail.com>
2020-08-21 01:25:55 +0000

    cli

--- lib/top_ten_anime/cli.rb
+++ lib/top_ten_anime/cli.rb
@@ -5,6 +5,7 @@ class TopTenAnime::CLI
     puts "Welcome to MyAnimeList!"
   get_anime_ranks
   list_anime_ranks
+  get_user_anime
   end 
 end 
   
@@ -12,7 +13,7 @@ end
     #binding.pry
      puts "" 
      puts "The top 10 anime of all time:"
-      @anime_ranks = ["1. Death Note (8.63 score)", "2. Shingeki no Kyojin (8.44 score)", "3. Sword Art Online (7.32 score)", "4. Fullmetal Alchemist: Brotherhood (9.22)", "5. One Punch Man(8.62 score)", "6. Tokyo Ghoul (7.83 score)", "7. Boku no Hero Academia (8.21 score)", "8. Steins;Gate (9.13 score)", "9. No Game No Life (8.26 score", "10. Naruto(7.88 score)"]
+      @anime_ranks = ["1. Death Note (8.63 score)", "2. Shingeki no Kyojin (8.44 score)", "3. Sword Art Online (7.32 score)", "4. Fullmetal Alchemist: Brotherhood (9.22)", "5. One Punch Man(8.62 score)", "6. Tokyo Ghoul (7.83 score)", "7. Boku no Hero Academia (8.21 score)", "8. Steins;Gate (9.13 score)", "9. No Game No Life (8.26 score)", "10. Naruto(7.88 score)"]
       end 
       
       def list_anime_ranks
@@ -21,3 +22,9 @@ end
           puts anime 
         end 
       end 
+      
+      def get_user_anime
+        binding.pry
+        chosen_anime = gets.strip 
+      end 
+    end 

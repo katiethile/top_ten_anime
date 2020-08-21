7052bd2f483950c55e06bca378aa458dccad955f
Katie Le <lethikatie@gmail.com>
2020-08-21 08:34:03 +0000

    add

---
+++ lib/top_ten_anime/anime.rb
@@ -0,0 +1,20 @@
+class WorldsBestRestaurants::Anime
+  attr_accessor :rank, :anime_review
+  attr_reader :anime_name, :sypnosis
+  
+  @@all = []
+  
+  def initialize(anime_name, sypnosis)
+    @anime_name = anime_name
+    @sypnosis = sypnosis
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
+  end
+end

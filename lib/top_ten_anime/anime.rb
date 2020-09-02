85464b9d722dc882502c75daae324594b53cc63e
Katie Le <lethikatie@gmail.com>
2020-09-02 20:15:46 +0000

    done

--- lib/top_ten_anime/anime.rb
+++ lib/top_ten_anime/anime.rb
@@ -12,12 +12,4 @@ class TopTenAnime::Anime
   def self.all 
     @@all 
   end 
-  
-  def self.find_by_name(title)
-    @@all.detect { |anime| anime.title == title}
-  end
-  
-  def self.find_by_rank(rank)
-    @@all.detect { |anime| anime.rank == rank}
-  end 
  end 
--- lib/top_ten_anime/scraper.rb
+++ lib/top_ten_anime/scraper.rb
@@ -9,20 +9,22 @@ class TopTenAnime::Scraper
       if title != ''
         link = row.css("td.tableTitle a").attribute('href').text
         TopTenAnime::Anime.new(title,rank,link)
+        #binding.pry 
       end
     end
+    #binding.pry
   end
   
   def self.get_synopsis(link)
     url = "https://www.anime-planet.com#{link}"
     website = Nokogiri::HTML(open(url))
-    puts website.css("div.pure-1.md-3-5 p").text
+    puts website.css("div.pure-1.md-3-5 p").text.strip
   end
   
   def self.get_reviews(link)
     url = "https://www.anime-planet.com#{link}" + "/reviews"
     website = Nokogiri::HTML(open(url))
-    puts website.css("section.pure-g").text.strip 
+    puts website.css("section.pure-g").text.strip#.gsub!(/(\r\n|\r|\n)/, "") 
   end
 end
 
